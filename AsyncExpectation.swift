b3d6d467b4e6927796afdf56d4a33b9205bf21c7
Alexander Smirnov <alecsmirnov93@gmail.com>
2025-04-16 08:10:21 +0500

    Add AsyncExpectation.swift file

---
+++ AsyncExpectation.swift
@@ -0,0 +1,38 @@
+//
+//  AsyncExpectation.swift
+//  AsyncExpectation
+//
+//  Created by Alexander Smirnov.
+//
+
+private enum Constants {
+    static let nanosecondsInSecond: UInt64 = 1_000_000_000
+}
+
+enum AsyncExpectationDurationKind {
+    case seconds(UInt64)
+    case nanoseconds(UInt64)
+}
+
+actor AsyncExpectation {
+    private var waitTask: Task<Void, Error>?
+
+    func fulfill() {
+        waitTask?.cancel()
+    }
+
+    func wait(durationKind: AsyncExpectationDurationKind) async throws {
+        let duration: UInt64 = switch durationKind {
+            case let .seconds(duration):
+                duration * Constants.nanosecondsInSecond
+            case let .nanoseconds(duration):
+                duration
+        }
+
+        waitTask = Task {
+            try await Task.sleep(nanoseconds: duration)
+        }
+
+        try await waitTask?.value
+    }
+}
